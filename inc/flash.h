4bd5db2c0c9649b462ef3c390bfe13cf1e1231a9
Ruslan-Borisov <78921891+Ruslan-Borisov@users.noreply.github.com>
2021-08-13 13:20:44 +0300

    add
    add

---
+++ inc/flash.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    flash.h
+  * @brief   This file contains all the function prototypes for
+  *          the flash.c file
+  ******************************************************************************
+	 * @attention
+ 
+  ******************************************************************************
+ */
+
+#ifndef __FLASH_H__
+#define __FLASH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FLASH_H_ */
+
+/************************ (C) BORISOV RUSLAN *****END OF FILE****/
