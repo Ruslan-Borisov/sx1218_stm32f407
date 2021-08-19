733d0dadf6bea5adbe4c93f725de7c19f6e68e26
Ruslan-Borisov <78921891+Ruslan-Borisov@users.noreply.github.com>
2021-08-19 13:40:02 +0300

    add
    add

--- main/main.c
+++ main/main.c
@@ -98,24 +98,17 @@ int main(void)
     SX1278_clearLoRaIrq();
     irqFlagEXTI_DIO0=0;
 	
-//----------------ПРИЕМНИК-------------------
-//#ifdef RX    
+  
 	 SX1278_LoRaEntryRx(&settings, 3, 3000);  // ПРИЕМНИК 
-//#endif	 
-//----------------ПРИЕМНИК-------------------	 
+	 
 
-// //++++++++++++++++ ПЕРЕДАТЧИК+++++++++++++++++	  
-//#ifdef TX 
-//    SX1278_transmit(&settings,bufTX, 3, 1000); // ПЕРЕДАТЧИК
-//#endif
-////++++++++++++++++ ПЕРЕДАТЧИК+++++++++++++++++		
+	
 
 
 	 while (1)
  {
     
-//----------------ПРИЕМНИК-------------------
-//#ifdef RX 
+
 	 if(irqFlagEXTI_DIO0 == 1){
 		
 		 SX1278_standby(&settings);
@@ -123,24 +116,7 @@ int main(void)
 		 SX1278_LoRaRxPacket(&settings);  
        SX1278_LoRaEntryRx(&settings, 3, 3000);
 	 }
-//#endif
-//----------------ПРИЕМНИК-------------------
-	 
-	 
-//++++++++++++++++ ПЕРЕДАТЧИК+++++++++++++++++	
-//#ifdef TX 	 
-//         timDelayMs(1000);
-//	     
-//	      if(irqFlagEXTI_DIO0 == 1){
-//		   bufTX[2] = (char)testcounter;
-//			SX1278_clearLoRaIrq();
-//			irqFlagEXTI_DIO0=0;
-//         SX1278_transmit(&settings,bufTX, 3, 1000);
-//		}
-//#endif
-//+++++++++++++++++ПЕРЕДАТЧИК++++++++++++++++++
-	 
- 
+
 	if(irqFlagUSART1_RX==1)
 		{
 			rw_test = USART1_buff_RX[0];
@@ -266,8 +242,7 @@ void init_LoRaSettings(LoRaSettings *settings){
 	settings-> Dio_3_0Map_40 = 0;               /*00 - FhssChangeChannel, 01 - FhssChangeChannel, 10 - FhssChangeChannel */
 
 
-//----------------ПРИЕМНИК-------------------	
-//#ifdef RX 	
+	
 	settings->RxTimeoutMask_11 = 1;
    settings->RxDoneMask_11 = 0;
    settings->PayloadCrcErrorMask_11 = 1;
@@ -276,21 +251,9 @@ void init_LoRaSettings(LoRaSettings *settings){
    settings->CadDoneMask_11=1;
    settings->FhssChangeChannelMask_11 = 1;
    settings->CadDetectedMask_11 = 1;
-//#endif	
-//----------------ПРИЕМНИК-------------------
+
 	
-//++++++++++++++++ ПЕРЕДАТЧИК+++++++++++++++++		 
-//#ifdef TX  
-//	settings->RxTimeoutMask_11 = 1;
-//   settings->RxDoneMask_11 = 1;
-//   settings->PayloadCrcErrorMask_11 = 1;
-//   settings->ValidHeaderMask_11 = 1;
-//   settings-> TxDoneMask_11 = 0;
-//   settings->CadDoneMask_11=1;
-//   settings->FhssChangeChannelMask_11 = 1;
-//   settings->CadDetectedMask_11 = 1;
-//#endif	
-//+++++++++++++++++ПЕРЕДАТЧИК++++++++++++++++++
+
 
 	/*RegIrqFlagsMask (0x11)*/	
   
